7425340acfb20a57526c503edf543ec8db3ec0d6
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-21 15:18:20 -0700

    Copy App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-
+const App = () => {
   return (
-    <>
-      <div>Router Redo 
-      </div>
-    </>
-  )
-}
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <a href="profile">Profile page</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default App
+export default App;
